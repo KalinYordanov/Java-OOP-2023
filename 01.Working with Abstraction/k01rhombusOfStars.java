4ea1dc3d9861da5b0e99ba18c8280d9e6e42a922
Kalin Yordanov <115161286+KalinYordanov@users.noreply.github.com>
2023-03-07 18:27:07 +0200

    Update and rename Main.java to k01rhombusOfStars.java

--- 01.Working with Abstraction/k01rhombusOfStars.java
+++ 01.Working with Abstraction/k01rhombusOfStars.java
@@ -2,7 +2,7 @@ package RhombusOfStars;
 
 import java.util.Scanner;
 
-public class Main {
+public class k01rhombusOfStars {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
