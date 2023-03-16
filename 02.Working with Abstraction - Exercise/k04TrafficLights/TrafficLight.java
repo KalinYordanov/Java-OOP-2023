7ba5e1c64c1fcda38f12b51eb69c7dbea573da13
KalinYordanov <kalinyordanov920815@gmail.com>
2023-03-16 15:15:40 +0200

    Create TrafficLight.java

---
+++ 02.Working with Abstraction - Exercise/k04TrafficLights/TrafficLight.java
@@ -0,0 +1,38 @@
+package k04TrafficLights;
+
+public class TrafficLight {
+    public enum Signal {
+        RED,
+        GREEN,
+        YELLOW;
+
+
+    }
+
+    private Signal signal;
+
+    public TrafficLight(Signal signal) {
+        this.signal = signal;
+    }
+
+    public void update() {
+        switch (this.signal) {
+
+            case RED:
+                this.signal = Signal.GREEN;
+                break;
+            case GREEN:
+                this.signal = Signal.YELLOW;
+                break;
+            case YELLOW:
+                this.signal = Signal.RED;
+                break;
+            default:
+                throw new IllegalStateException("Unknown state " + this.signal);
+        }
+    }
+
+    public Signal getSignal() {
+        return signal;
+    }
+}
