d4cea5a508aedc0f4b2c08072a3265e753eb81a1
KalinYordanov <kalinyordanov920815@gmail.com>
2023-03-17 15:02:44 +0200

    Create Cords.java

---
+++ 02.Working with Abstraction - Exercise/k05JediGalaxy/Cords.java
@@ -0,0 +1,11 @@
+package k05JediGalaxy.jediGalaxy;
+
+public class Cords {
+    public int row;
+    public int col;
+
+    public Cords(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
