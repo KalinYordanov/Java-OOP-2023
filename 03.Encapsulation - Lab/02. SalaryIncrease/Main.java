398d64f35373f46d9919f05a0a100bf545fa32fd
KalinYordanov <kalinyordanov920815@gmail.com>
2023-03-23 23:19:10 +0200

    Create Main.java

---
+++ 03.Encapsulation - Lab/02. SalaryIncrease/Main.java
@@ -0,0 +1,33 @@
+package SalaryIncrease;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int numberOfPeople = Integer.parseInt(reader.readLine());
+
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            String[] input = reader.readLine().split("\\s+");
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+
+            Person personToAdd = new Person(firstName, lastName, age, salary);
+            people.add(personToAdd);
+        }
+
+        double bonus = Double.parseDouble(reader.readLine());
+        for (Person person : people) {
+            person.increaseSalary(bonus);
+            System.out.println(person);
+        }
+    }
+}
