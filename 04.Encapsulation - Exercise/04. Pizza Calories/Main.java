21f04398ef6b8a5ef45adb8f3a72180bedaf062a
KalinYordanov <kalinyordanov920815@gmail.com>
2023-04-04 12:09:07 +0300

    Create Main.java

---
+++ 04.Encapsulation - Exercise/04. Pizza Calories/Main.java
@@ -0,0 +1,38 @@
+package PizzaCalories;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] pizzaInfo = scanner.nextLine().split("\\s+");
+        String pizzaName = pizzaInfo[1];
+        int numberOfToppings = Integer.parseInt(pizzaInfo[2]);
+
+        String[] doughInfo = scanner.nextLine().split("\\s+");
+        String flourType = doughInfo[1];
+        String bakingTech = doughInfo[2];
+        double weight = Double.parseDouble(doughInfo[3]);
+
+        try {
+            Pizza pizza = new Pizza(pizzaName, numberOfToppings);
+            Dough dough = new Dough(flourType, bakingTech, weight);
+            pizza.setDough(dough);
+
+            String command = scanner.nextLine();
+            while (!command.equals("END")) {
+                String[] toppingInfo = command.split("\\s+");
+                String name = toppingInfo[1];
+                double toppingWeight = Double.parseDouble(toppingInfo[2]);
+                Topping topping = new Topping(name, toppingWeight);
+                pizza.addTopping(topping);
+
+                command = scanner.nextLine();
+            }
+            System.out.printf("%s - %.2f", pizza.getName(), pizza.getOverallCalories());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
