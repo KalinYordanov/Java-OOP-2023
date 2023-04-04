d2a151f8b2ee6c68a7998cb063adf80c022bf5bd
KalinYordanov <kalinyordanov920815@gmail.com>
2023-04-04 19:57:24 +0300

    Create Main.java

---
+++ 05.Inheritance - Lab/04. Stack Of Strings/Main.java
@@ -0,0 +1,11 @@
+package StackOfStrings;
+
+public class Main {
+    public static void main(String[] args) {
+        StackOfStrings stackOfStrings = new StackOfStrings();
+        stackOfStrings.push("fas12da");
+        stackOfStrings.push("22dsad");
+        stackOfStrings.push("dsasdafc1");
+        System.out.println(stackOfStrings.peek());
+    }
+}
