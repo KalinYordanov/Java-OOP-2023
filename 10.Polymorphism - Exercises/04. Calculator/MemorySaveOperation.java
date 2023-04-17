8f65cf551bb20d2a4c9e5f62707337002411e662
KalinYordanov <kalinyordanov920815@gmail.com>
2023-04-17 17:19:21 +0300

    04. Calculator

---
+++ 10.Polymorphism - Exercises/04. Calculator/MemorySaveOperation.java
@@ -0,0 +1,30 @@
+package calculator;
+
+public class MemorySaveOperation implements Operation {
+    private int lastSaved;
+    private boolean completed;
+    public Memory memory;
+
+    public MemorySaveOperation(Memory memory) {
+        this.memory = memory;
+
+    }
+
+    @Override
+    public void addOperand(int operand) {
+        memory.save(operand);
+        lastSaved = operand;
+        this.completed = true;
+
+    }
+
+    @Override
+    public int getResult() {
+        return this.lastSaved;
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return this.completed;
+    }
+}
